26a6ddb4e88d58103240ed2ba1e94380fd48505b
Fer Martínez <ricardofernandomartinez0@gmail.com>
2024-07-04 23:25:09 -0300

    Se creó la app de tasks

--- gestion_tareas/gestion_tareas/urls.py
+++ gestion_tareas/gestion_tareas/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from apps.tasks import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.helloword),
 ]
